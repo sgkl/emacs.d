0211c35b71818ff1100c8a642a3da1db922d2a18
sgk <sunguanke11@mails.ucas.edu.cn>
2021-06-17 18:26:36 +0800

    add theme configuration

--- init.el
+++ init.el
@@ -6,11 +6,24 @@
 ;			 ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
 ;			 ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
 
+;; Adjust garbage collection thresholds during startup
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+      (setq gc-cons-threshold init-gc-cons-threshold))
+;;  (add-hook 'emacs-startup-hook
+;;	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;; Bootstrap config
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 
 (require 'init-elpa)
 
 ;(package-refresh-contents)
 (require-package 'diminish)
+
+
+;(require 'init-themes)
 
 (require 'init-company)
 
