dce298f75668d3b005dc20270966a0809634359c
sgk <sunguanke11@mails.ucas.edu.cn>
2021-06-22 16:25:03 +0800

    更新org-mode rust-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -26,5 +26,49 @@
       org-export-kill-product-buffer-when-displayed t
       org-tags-column 80)
 
+;; Re-align tags when window shape changes
+(with-eval-after-load 'org-agenda
+  (add-hook 'org-agenda-mode-hook
+            (lambda ()
+              (add-hook 'window-configuration-change-hook 'org-agenda-align-tags nil t))))
+
+(maybe-require-package 'writeroom-mode)
+
+(define-minor-mode prose-mode
+  "Set up a buffer for prose editing. This enables or modifies a number of settings so that the experience of editing prose is a little more like that of a typical word processor."
+  nil " Prose" nil
+  (if prose-mode
+      (progn
+        (when (fboundp 'writeroom-mode)
+          (writeroom-mode 1))
+        (setq truncate-lines nil)
+        (setq word-wrap t)
+        (setq cursor-type 'bar)
+        (when (eq major-mode 'org)
+          (kill-local-variable 'buffer-face-mode-face))
+        (buffer-face-mode 1)
+        (setq-local blink-cursor-interval 0.6)
+        (setq-local show-trailing-whitespace nil)
+        (setq-local line-spacing 0.2)
+        (setq-local electric-pair-mode nil)
+        (ignore-errors (flyspell-mode 1))
+        (visual-line-mode 1))
+    (kill-local-variable 'truncate-lines)
+    (kill-local-variable 'word-wrap)
+    (kill-local-variable 'cursor-type)
+    (kill-local-variable 'blink-cursor-interval)
+    (kill-local-variable 'show-trailing-whitespace)
+    (kill-local-variable 'line-spacing)
+    (kill-local-variable 'electric-pair-mode)
+    (buffer-face-mode -1)
+    (flyspell-mode -1)
+    (visual-line-mode -1)
+    (when (fboundp 'writeroom-mode)
+      (writeroom-mode 0))))
+
+(setq org-support-shift-select t)
+
+
+
 (provide 'init-org)
 ;;; init-org.el ends here
