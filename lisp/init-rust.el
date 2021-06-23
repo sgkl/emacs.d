d70cf895266f67e50de0c17d0f997ef6cba5a43c
sgk <sunguanke11@mails.ucas.edu.cn>
2021-06-24 07:46:25 +0800

    use lsp in rust mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -3,8 +3,10 @@
 ;;; Code:
 
 (when (maybe-require-package 'rust-mode)
-  (when (maybe-require-package 'racer)
-    (add-hook 'rust-mode-hook #'racer-mode))
+  ;;(when (maybe-require-package 'racer)
+  ;;  (add-hook 'rust-mode-hook #'racer-mode))
+  (when (maybe-require-package 'lsp-mode)
+    (add-hook 'rust-mode-hook #'lsp-deferred))
   (when (maybe-require-package 'company)
     (add-hook 'racer-mode-hook #'company-mode))
   (when (maybe-require-package 'cargo)
@@ -15,6 +17,8 @@
   (with-eval-after-load 'rust-mode
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
 
+;;(when (maybe-require-package 'rustic))
 
+;;(setq rustic-lsp-client 'rls)
 
 (provide 'init-rust)
